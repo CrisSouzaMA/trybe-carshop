7caafd5df31a98bc8dd232a320e7494305b3f261
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-07-11 19:58:27 -0300

    Feat: implementa update na classe abstrata da controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -44,5 +44,10 @@ abstract class Controller<T> {
     req: Request<{ id: string; }>,
     res: Response<T | ResponseError>
   ): Promise<typeof res>;
+
+  abstract update(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
 }
 export default Controller;
