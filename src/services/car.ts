bdc6facf71dbf21c9d0615c2cceb4d4e14e13c7c
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-07-11 19:57:55 -0300

    Feat: implementa update na service de car

--- src/services/car.ts
+++ src/services/car.ts
@@ -25,6 +25,22 @@ class CarService extends Service<Car> {
     }
     return checkId;
   };
+
+  update = async (id: string, obj: Car):
+  Promise< Car | ServiceError | null> => {
+    if (id.length < 24) {
+      return null;
+    }
+    const parsed = CarSchema.safeParse(obj);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    const newCar = await this.model.update(id, obj);
+    if (!newCar) {
+      throw new Error('Object not found');
+    }
+    return newCar;
+  };
 }
 
 export default CarService;
