84536898730924df9063f2e8f2fc7447f17abd4a
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-07-11 21:09:36 -0300

    Feat: cria testes para camada model

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -1,2 +1,23 @@
-// import { expect } from 'chai';
-// import mongoose from 'mongoose';
+import { expect } from 'chai';
+import sinon, { SinonStub } from 'sinon';
+import CarModel from '../../../models/car';
+import { Model } from 'mongoose';
+import MocksCar from '../mocks/mocksCar';
+
+describe('Testa a model de carro', () => {
+  describe('Testa função que cria novo carro', () => {
+    before(() => {
+      sinon.stub(Model, 'create').resolves(MocksCar.mockCar)
+    });
+
+    after(() => {
+      (Model.create as SinonStub). restore();
+    });
+
+    it('Carro criado com sucesso', async () => {
+      const carModel = new CarModel();
+      const createdCar = await carModel.create(MocksCar.mockBody);
+      expect(createdCar).to.be.equal(MocksCar.mockCar);
+    });
+  });
+});
