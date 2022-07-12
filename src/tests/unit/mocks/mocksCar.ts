f148536249cc3880e2ff66f194630e6864193374
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-07-11 21:09:11 -0300

    Feat: cria mocks para implementar testes

---
+++ src/tests/unit/mocks/mocksCar.ts
@@ -0,0 +1,27 @@
+const mockCar = {
+  "model": "Carro",
+	"year": 2010,
+	"color": "branco",
+	"buyValue": 20000,
+	"doorsQty": 4,
+	"seatsQty": 5,
+	"_id": "62c760648ae2a532c6797ffc",
+	"__v": 0
+}
+
+const mockBody = {
+  "model": "Carro 2",
+  "year": 2006,
+  "color": "black",
+  "buyValue": 40000,
+  "seatsQty": 5,
+  "doorsQty": 4
+}
+
+const mockCarId = '62c760648ae2a532c6797ffc';
+
+export default {
+  mockCar, 
+  mockBody,
+  mockCarId,
+} 
